4eba45d4ef6e977df7563a438755d37ed830a887
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-03-12 13:11:20 -0400

    theme patches

--- source/app/service-providers/config/get-config-template.ts
+++ source/app/service-providers/config/get-config-template.ts
@@ -60,6 +60,7 @@ export interface ConfigOptions {
   appLang: string
 
   darkMode: boolean
+  darkModeEditor: 'match'|'light'|'dark'
   autoDarkMode: 'off'|'system'|'schedule'
   autoDarkModeStart: string
   autoDarkModeEnd: string
@@ -145,6 +146,9 @@ export interface ConfigOptions {
     indentWithTabs: boolean
     alwaysIndentLineOnTab: boolean
     fontSize: number
+    pageCenter: boolean
+    pageBorder: boolean
+    pageSize: number
     countChars: boolean
     inputMode: 'default'|'vim'|'emacs'
     boldFormatting: '**'|'__'
@@ -300,6 +304,7 @@ export function getConfigTemplate (): ConfigOptions {
     attachmentExtensions: [],
     // UI related options
     darkMode: nativeTheme.shouldUseDarkColors,
+    darkModeEditor: 'match', // Possible values: 'match', 'light', 'dark'
     alwaysReloadFiles: true, // Should Zettlr automatically load remote changes?
     autoDarkMode: 'system', // Possible values: 'off', 'system', 'schedule', 'auto'
     autoDarkModeStart: '21:00', // Switch into dark mode at this time
@@ -359,6 +364,9 @@ export function getConfigTemplate (): ConfigOptions {
       indentWithTabs: false,
       alwaysIndentLineOnTab: false, // Whether `Tab` always indents the current line
       fontSize: 18, // The editor's font size in pixels
+      pageCenter: true,
+      pageBorder: true,
+      pageSize: 0,
       countChars: false, // Set to true to enable counting characters instead of words
       inputMode: 'default', // Can be default, vim, emacs
       boldFormatting: '**', // Can be ** or __
--- source/win-preferences/schema/editor.ts
+++ source/win-preferences/schema/editor.ts
@@ -307,6 +307,24 @@ export function getEditorFields (config: ConfigOptions): PreferencesFieldset[] {
         },
         { type: 'separator' },
         {
+          type: 'checkbox',
+          label: trans('Center page'),
+          model: 'editor.pageCenter'
+        },
+        {
+          type: 'checkbox',
+          label: trans('Page border'),
+          model: 'editor.pageBorder'
+        },
+        {
+          type: 'number',
+          label: trans('Page width'),
+          inline: true,
+          min: 0,
+          model: 'editor.pageSize'
+        },
+        { type: 'separator' },
+        {
           type: 'number',
           label: trans('Tab size (in number of spaces)'),
           inline: true,

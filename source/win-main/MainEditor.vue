b1f9d141da9a7f89365b6fc50bbd7df815cabf73
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-03-12 13:11:44 -0400

    theme patches

--- source/win-main/MainEditor.vue
+++ source/win-main/MainEditor.vue
@@ -289,7 +289,8 @@ const editorConfiguration = computed<EditorConfigOptions>(() => {
     theme: display.theme,
     highlightWhitespace: editor.showWhitespace,
     showMarkdownLineNumbers: editor.showMarkdownLineNumbers,
-    countChars: editor.countChars
+    countChars: editor.countChars,
+    customHighlighter: editor.customHighlighter,
   } satisfies EditorConfigOptions
 })
 

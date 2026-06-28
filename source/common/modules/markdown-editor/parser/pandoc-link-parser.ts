51300032892e15c7c0994539d04357f9d2fc69cd
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-06-28 13:45:30 -0400

    pandoc link fixes

--- source/common/modules/markdown-editor/parser/pandoc-link-parser.ts
+++ source/common/modules/markdown-editor/parser/pandoc-link-parser.ts
@@ -18,52 +18,83 @@
  * END HEADER
  */
 
-import type { DelimiterType, InlineParser } from '@lezer/markdown'
+import { type InlineParser, type DelimiterType, InlineContext } from '@lezer/markdown'
 
 const PandocLinkDelimiter: DelimiterType = {}
 
-const linkClosingRe = /^\]\((?<url>.+)\)/
+const linkClosingRe = /^\](?:\((?<url>.+)\)|\[(?<label>.*)\])/
 
 const linkTitleRe = /(?:^|[ \t]+)(?:"(?<double>(?:\\.|[^"])+)"|'(?<single>(?:\\.|[^'])+)'|\((?<parens>(?:\\.|[^\)])+)\))$/d
+
+// Link destinations can contain nested and balanced internal
+// parenthesis (for URLs) and brackets (for labels). So we need
+// to walk the destination text to find the correct, balanced
+// closing mark since the regex is a greedy match.
+function findEndOfLink (destination: string, opening: string, closing: string): string {
+  let depth = 0
+  let stop = 0
+
+  while (stop <= destination.length) {
+    const char = destination.charAt(stop)
+
+    // Found the closing parenthesis
+    if (char === closing && depth === 0) { break }
+
+    if (char === closing) { depth-- }
+    if (char === opening) { depth++ }
+
+    // Skip the next character if the current
+    // one is an escape character
+    if (char === '\\') { stop++ }
+
+    stop++
+  }
+
+  return destination.substring(0, stop)
+}
 
 export const pandocLinkParser: InlineParser = {
   name: 'pandoc-link-parser',
   before: 'Link',
   parse: (ctx, next, pos) => {
     if (next === 91) { // 91 === '['
-      ctx.addDelimiter(PandocLinkDelimiter, pos, pos + 1, true, false)
-
-      // Return -1 so that the default link parser can add delimiters
-      return -1
+      // Add the default link delimiter so that the
+      // URL parser knows when to terminate early
+      // due to internal logic.
+      ctx.addDelimiter(InlineContext.linkStart, pos, pos + 2, true, false)
+      return ctx.addDelimiter(PandocLinkDelimiter, pos, pos + 1, true, false)
     }
 
     if (next === 33 && ctx.char(pos + 1) === 91) { // 33 === '!', 91 === '['
-      ctx.addDelimiter(PandocLinkDelimiter, pos, pos + 2, true, false)
-
-      // Return -1 so that the default link parser can add delimiters
-      return -1
+      // Add the default image delimiter so that the
+      // URL parser knows when to terminate early
+      // due to internal logic.
+      ctx.addDelimiter(InlineContext.imageStart, pos, pos + 2, true, false)
+      return ctx.addDelimiter(PandocLinkDelimiter, pos, pos + 2, true, false)
     }
 
     if (next !== 93) { // 93 === ']'
       return -1
     }
 
-    // If there's no valid URL, return so the default parser can handle
-    // the other link types.
-    const match = linkClosingRe.exec(ctx.text.slice(pos - ctx.offset))
-    if (!match?.groups) { return -1 }
-
     const opening = ctx.findOpeningDelimiter(PandocLinkDelimiter)
-    if (opening === null) { return -1 }
+    if (opening === null) {
+      return -1
+    }
 
     const delim = ctx.getDelimiterAt(opening)
-    if (!delim) { return -1 }
+    if (!delim) {
+      return -1
+    }
+
+    let linkEnd = pos + 1
+    // For some reason, calling `takeContent` does not *include* the element
+    // at `opening`, so it will remain in the tree and cause issues if there are
+    // additional closing brackets after the currently detected one. We need to
+    // `opening - 1` so we take the content including the opening delimiter.
+    let linkContents = ctx.takeContent(opening - 1)
 
     const isLink = delim.to - delim.from === 1
-    let linkContents = ctx.takeContent(opening)
-
-    ctx.addDelimiter(PandocLinkDelimiter, pos, pos + 1, false, true)
-
     // Remove nested links, which are invalid
     if (isLink) {
       const linkType = ctx.parser.nodeSet.types.find(node => node.is('Link'))?.id
@@ -71,49 +102,45 @@ export const pandocLinkParser: InlineParser = {
       linkContents = linkContents.filter(el => el.type !== linkType && el.type !== urlType)
     }
 
-    // The url may contain additional parenthesis, so we need
-    // to count the internal ones to track potential matching pairs
-    // to find the external matching closing one.
-    let depth = 0
-    let stop = 0
-    let url = match.groups.url
+    const linkDest = []
+    const match = linkClosingRe.exec(ctx.text.slice(pos - ctx.offset))
 
-    while (stop <= url.length) {
-      const char = url.charAt(stop)
+    if (match?.groups?.url !== undefined) {
+      let url = findEndOfLink(match.groups.url, '(', ')')
+      let destination = url
 
-      // Found the closing parenthesis
-      if (char === ')' && depth === 0) { break }
+      const urlContents = []
+      const title = linkTitleRe.exec(destination)
 
-      if (char === ')') { depth-- }
-      if (char === '(') { depth++ }
-      stop++
+      if (title?.indices?.groups) {
+        destination = url.substring(0, title.index)
+
+        const linkTitleIndices = title.indices.groups.double ?? title.indices.groups.single ?? title.indices.groups.parens
+        urlContents.push(ctx.elt('LinkTitle', pos + 2 + linkTitleIndices[0], pos + 2 + linkTitleIndices[1]))
+      }
+
+      urlContents.unshift(ctx.elt('URL', pos + 2, pos + 2 + destination.length))
+
+      const openingUrlMark = ctx.elt('LinkMark', pos + 1, pos + 2)
+      const closingUrlMark = ctx.elt('LinkMark', pos + 2 + url.length, pos + 3 + url.length)
+
+      linkDest.push(openingUrlMark, ...urlContents, closingUrlMark)
+      // Closing marks (2) + url text
+      linkEnd += 2 + url.length
+    } else if (match?.groups?.label !== undefined) {
+      let label = findEndOfLink(match.groups.label, '[', ']')
+
+      // The label marks `[` and `]` are not parsed separately
+      linkDest.push(ctx.elt('LinkLabel', pos + 2, pos + 3 + label.length))
+      linkEnd += 2 + label.length
     }
-
-    url = url.substring(0, stop)
-
-    let destination = url
-
-    const urlContents = []
-    const title = linkTitleRe.exec(destination)
-
-    if (title?.indices?.groups) {
-      destination = url.substring(0, title.index)
-
-      const linkTitleIndices = title.indices.groups.double ?? title.indices.groups.single ?? title.indices.groups.parens
-      urlContents.push(ctx.elt('LinkTitle', pos + 2 + linkTitleIndices[0], pos + 2 + linkTitleIndices[1]))
-    }
-
-    urlContents.unshift(ctx.elt('URL', pos + 2, pos + 2 + destination.length))
-
-    const openingUrlMark = ctx.elt('LinkMark', pos + 1, pos + 2)
-    const closingUrlMark = ctx.elt('LinkMark', pos + 2 + url.length, pos + 3 + url.length)
 
     const openingMark = ctx.elt('LinkMark', delim.from, delim.to)
     const closingMark = ctx.elt('LinkMark', pos, pos + 1)
 
     // This child node structure mirrors the codemirror Link structure
-    const children = [ openingMark, ...linkContents, closingMark, openingUrlMark, ...urlContents, closingUrlMark ]
+    const children = [ openingMark, ...linkContents, closingMark, ...linkDest ]
 
-    return ctx.addElement(ctx.elt(isLink ? 'Link' : 'Image', delim.from, pos + 3 + url.length, children))
+    return ctx.addElement(ctx.elt(isLink ? 'Link' : 'Image', delim.from, linkEnd, children))
   }
 }

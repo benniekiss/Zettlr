f373aaf3903e5bb9dce538007cbc43370044c9b6
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-07-01 21:01:55 -0400

    fix handling of matches at line start

--- source/common/modules/markdown-editor/commands/autocorrect.ts
+++ source/common/modules/markdown-editor/commands/autocorrect.ts
@@ -125,7 +125,7 @@ function parseAutocorrectKey (key: string, matchWholeWords: boolean): RegExp|und
   // character. Instead, check for a preceding non-word character using a
   // lookbehind to exclude the character from the match and assert the correct
   // semantics for `matchWholeWords`
-  const prefix = matchWholeWords ? '(?<=\W)' : ''
+  const prefix = matchWholeWords ? '(?<=^|\\W)' : ''
 
   let body = key.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
   let flags = ''
